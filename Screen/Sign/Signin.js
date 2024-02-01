3d21e081431b33b903219f696ddf31963bdf7413
rzl0zi <program.yunji@gmail.com>
2024-02-01 23:08:09 +0900

    chore: 카카오 인가코드 백엔드 post부분 수정
    백엔드에 인가코드가 null로 찍히는 이슈 발생. body: JSON.stringify({ code }), body이 코드가 빠져 있었음.

--- Screen/Sign/Signin.js
+++ Screen/Sign/Signin.js
@@ -13,25 +13,50 @@ const Signin = () => {
     if (searchIdx !== -1) {
       const code = url.substring(searchIdx + exp.length);
       console.log("인가 코드", code);
-      // 로그인 성공시 Navigate to 'ClubStart'  
+
+      // 인가코드를 백엔드로 보내기 위한 
+      sendAuthorizationCodeToBackend(code);
+      // 로그인 성공시 Navigate to 'ClubStart' 
       navigation.navigate('ClubStart')
       return false;
     }
     return true;
+
+  };
+  const sendAuthorizationCodeToBackend = async (code) => {
+    const backendEndpoint = 'http://ec2-13-125-164-0.ap-northeast-2.compute.amazonaws.com:8080/login/oauth2/code/kakao';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ code }),
+    };
+
+    try {
+      console.log ('Request Body:',options.body)
+      const response = await fetch(backendEndpoint, options);
+      const data = await response.json();
+      console.log('Backend response:', data);
+
+    
+    } catch (error) {
+      console.error('Error sending authorization code to backend:', error);
+    }
   };
 
-  
-    return (
-      <WebView
-        className="flex"
-        // 웹뷰 보여줄 페이지 주소
-        source={{
-          uri: KAKAO_AUTH_URL,
-        }}
-        onShouldStartLoadWithRequest={handleShouldStartLoad}
-      />
-    );
-  
+
+  return (
+    <WebView
+      className="flex"
+      // 웹뷰 보여줄 페이지 주소
+      source={{
+        uri: KAKAO_AUTH_URL,
+      }}
+      onShouldStartLoadWithRequest={handleShouldStartLoad}
+    />
+  );
+
 
 
 }
