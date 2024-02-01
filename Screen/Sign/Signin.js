4dd0329c72ed662412d1afecc5a75dad9a77de4b
rzl0zi <program.yunji@gmail.com>
2024-02-01 20:29:39 +0900

    chore: 카카오 로그인 수정

--- Screen/Sign/Signin.js
+++ Screen/Sign/Signin.js
@@ -1,49 +1,39 @@
+import { useState } from "react";
+import { WebView, WebViewNavigation } from "react-native-webview";
 import { KAKAO_AUTH_URL } from "../../OAuth";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
-import React from 'react';
-import { View, LogBox, Text } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { useNavigation } from '@react-navigation/native';
 
-LogBox.ignoreLogs(['Remote debugger']);
-const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage("message from webView")`;
-
-const Signin = ({ navigation: { navigate } }) => {
-    
-    const getCode = async (codeUrl) => {
-        const exp = "code=";
-        const searchIdx = codeUrl.indexOf(exp);
-        if (searchIdx !== -1) {
-          const code = codeUrl.substring(searchIdx + exp.length);
-          console.log(code);
-          
-          // Assuming you want to navigate to the "ClubStart" screen upon successful login
-          navigate("ClubStart");
-        }
-      };
-
-    return (
-      <View style={{ flex: 1 }}>
-        <Text>로그인 화면</Text>
-        <WebView
-            
-          originWhitelist={['*']}
-          scalesPageToFit={true}
-          style={{ marginTop: 30 }}
-          source={{
-            uri: KAKAO_AUTH_URL,
-          }}
-          userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-          injectedJavaScript={INJECTED_JAVASCRIPT}
-          javaScriptEnabled={true}
-          onMessage={event => {
-            const data = event.nativeEvent.url;
-            getCode(data);
-          }}
-          
-        />
-      </View>
-    );
+const Signin = () => {
+  const navigation = useNavigation();
+  const handleShouldStartLoad = (event: WebViewNavigation) => {
+    const url = event.url;
+    // url에 붙어오는 code= 가있다면 뒤부터 parse하여 인가 코드 get
+    const exp = "code=";
+    const searchIdx = url.indexOf(exp);
+    if (searchIdx !== -1) {
+      const code = url.substring(searchIdx + exp.length);
+      console.log("인가 코드", code);
+      // 로그인 성공시 Navigate to 'ClubStart'  
+      navigation.navigate('ClubStart')
+      return false;
+    }
+    return true;
   };
+
   
-  export default Signin;
+    return (
+      <WebView
+        className="flex"
+        // 웹뷰 보여줄 페이지 주소
+        source={{
+          uri: KAKAO_AUTH_URL,
+        }}
+        onShouldStartLoadWithRequest={handleShouldStartLoad}
+      />
+    );
+  
+
+
+}
+
+export default Signin;
